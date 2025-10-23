a23600be6c3df091d967a555424aa745c1893717
Senurcreate <Shr1212ni@gmail.com>
2025-10-23 11:12:09 +0530

    Added firebase and redux

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,28 +1,21 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import LoginPage from "./pages/LoginPage";
 
 function App() {
-  return (
-    <Router>
-      <MainLayout />
-    </Router>
-  );
+  return <MainLayout />;
 }
 
-// Separate component so we can use useLocation()
+// useLocation only works inside a Router (we put BrowserRouter in main.jsx)
 function MainLayout() {
   const location = useLocation();
-
-  // Show Navbar only on home page
   const showNavbar = location.pathname === "/";
 
   return (
     <div className="App">
-      {showNavbar && <Navbar />}  {/* only show Navbar on '/' */}
-      
+      {showNavbar && <Navbar />}
       <Routes>
         <Route path="/" element={<Hero />} />
         <Route path="/login" element={<LoginPage />} />
