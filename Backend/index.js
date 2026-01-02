445f6182b1338397c7f85df66155ae64531afd3b
Senurcreate <Shr1212ni@gmail.com>
2026-01-02 08:10:04 +0530

    Update bookDetails.scss & book routes

--- Backend/index.js
+++ Backend/index.js
@@ -28,7 +28,7 @@ app.use(morgan('dev')); // HTTP request logging
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-  origin: ['http://localhost:5173'],
+  origin:true, //['http://localhost:5173'],
   credentials: true
 }))
 
